c510c09815d0f475f4ff93a1c1e54a4454ef64f9
ltfw <33441297+ltfw@users.noreply.github.com>
2019-05-25 18:02:55 +0700

    Update add_skill.php

--- test_arkademy/6/add_skill.php
+++ test_arkademy/6/add_skill.php
@@ -6,4 +6,4 @@ $id = $_POST['user_id'];
 
 mysqli_query($conn, "INSERT INTO skills(name,user_id) VALUES('$name','$id')");
 
-header('Location: http://localhost/test2/6');
+header('Location: http://localhost/test_arkademy/6');
